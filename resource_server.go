b1a60a9042eeb1d66c7dd37422971cd1d9e4c532
chris rhodes <christopher.rhodes@banno.com>
2016-09-01 14:28:32 -0500

    add a bunch of error reporting

--- resource_server.go
+++ resource_server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/hashicorp/terraform/helper/schema"
 	"io/ioutil"
 	"net/http"
@@ -49,13 +50,28 @@ func resourceServerDelete(d *schema.ResourceData, m interface{}) error {
 	project := d.Get("project").(string)
 	hostname := d.Get("hostname").(string)
 
-	bearer := get_token(key_id, key_secret, key_team)
-	list := get_servers(bearer, key_team, project)
+	bearer, err := get_token(key_id, key_secret, key_team)
+	if err != nil {
+		return fmt.Errorf("Error getting token key_id:%s key_team:%s error:%v", key_id, key_team, err)
+	}
+
+	list, err := get_servers(bearer, key_team, project)
+
+	if err != nil {
+		return fmt.Errorf("Error getting server list. key_team:%s error:%v", key_team, err)
+	}
 
 	ids := get_ids_for_hostname(hostname, list)
 
+	if len(ids) == 0 {
+		return fmt.Errorf("Error, ScaleFT api returned no servers that matched hostname:%s", hostname)
+	}
+
 	for _, id := range ids {
-		delete_server(bearer, key_team, project, id)
+		err := delete_server(bearer, key_team, project, id)
+		if err != nil {
+			return fmt.Errorf("Error deleting server at id:%s and key_team:%s project: %s error:%v", id, key_team, project, err)
+		}
 	}
 
 	return nil
@@ -90,7 +106,7 @@ type Servers struct {
 	List []*Server `json:"list"`
 }
 
-func get_token(key_id string, key_secret string, key_team string) string {
+func get_token(key_id string, key_secret string, key_team string) (string, error) {
 	p := &Body{key_id, key_secret}
 	jsonStr, err := json.Marshal(p)
 	url := api + key_team + "/service_token"
@@ -100,14 +116,14 @@ func get_token(key_id string, key_secret string, key_team string) string {
 	resp, err := client.Do(req)
 
 	if err != nil {
-		panic(err)
+		return "error", fmt.Errorf("Error getting token key_id:%s key_team:%s status:%s error:%v", key_id, key_team, string(resp.Status), err)
 	}
 
 	defer resp.Body.Close()
 	b := Bearer{}
 	json.NewDecoder(resp.Body).Decode(&b)
 
-	return b.Bearer_token
+	return b.Bearer_token, err
 }
 
 func get_logs(bearer_token string, key_team string) string {
@@ -125,7 +141,7 @@ func get_logs(bearer_token string, key_team string) string {
 	return s
 }
 
-func get_servers(bearer_token string, key_team string, project string) Servers {
+func get_servers(bearer_token string, key_team string, project string) (Servers, error) {
 	client := &http.Client{}
 	url := api + key_team + "/projects/" + project + "/servers"
 	req, err := http.NewRequest("GET", url, nil)
@@ -133,7 +149,7 @@ func get_servers(bearer_token string, key_team string, project string) Servers {
 	req.Header.Set("Authorization", "Bearer "+bearer_token)
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		fmt.Errorf("Error listing servers: key_team:%s project: %s status:%s error:%v", key_team, project, string(resp.Status), err)
 	}
 
 	s := struct {
@@ -141,10 +157,10 @@ func get_servers(bearer_token string, key_team string, project string) Servers {
 	}{nil}
 
 	json.NewDecoder(resp.Body).Decode(&s)
-	return s
+	return s, err
 }
 
-func delete_server(bearer_token string, key_team string, project string, server_id string) string {
+func delete_server(bearer_token string, key_team string, project string, server_id string) error {
 	client := &http.Client{}
 	url := api + key_team + "/projects/" + project + "/servers/" + server_id
 	req, err := http.NewRequest("DELETE", url, nil)
@@ -152,10 +168,9 @@ func delete_server(bearer_token string, key_team string, project string, server_
 	req.Header.Set("Authorization", "Bearer "+bearer_token)
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("Error deleting server:%s status:%s error:%v", server_id, string(resp.Status), err)
 	}
-	s := string(resp.Status)
-	return s
+	return nil
 }
 
 func get_ids_for_hostname(hostname string, server_list Servers) []string {
